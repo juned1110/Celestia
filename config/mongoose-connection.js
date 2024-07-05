b1c59466e1c601a38ba75aa582f2a2568c101044
juned1110 <junedking1110@gmail.com>
2024-07-05 10:19:56 +0530

    Mongoose connection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/celestia")
+  .connect(
+    "mongodb+srv://junedk1110:gxmBoTmX12TP1WFB@cluster0.x3sbjs3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(function () {
     console.log("connected");
   })
