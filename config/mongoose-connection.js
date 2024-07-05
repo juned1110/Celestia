a02441868e04e0a5031842a7f2a511813e63e681
juned1110 <junedking1110@gmail.com>
2024-07-05 11:57:20 +0530

    owner routes created

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const config = require("config");
+const debug = require("debug");
+
+const dbgr = debug("development:mongoose");
+
+const mongoUri = `${config.get("MONGODB_URI")}/celestia`;
 
 mongoose
-  .connect(`${config.get("MONGODB_URI")}/celestia`)
+  .connect(mongoUri)
   .then(function () {
-    console.log("connected");
+    dbgr("connected");
   })
   .catch(function (err) {
-    console.log(err);
+    dbgr("error", err);
   });
 
 module.exports = mongoose.connection;
